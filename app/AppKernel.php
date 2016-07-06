b0484461bd9e3a60e18f8eb0912f5196330d0265
Antonio Jiménez <ajimenez.bf@gmail.com>
2016-07-07 01:28:49 +0200

    Added NelmioApiDoc

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
