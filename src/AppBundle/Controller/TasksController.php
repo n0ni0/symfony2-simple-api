db4d52fe26d70e5b584e19202c13563b3c2fe727
Antonio Jiménez <ajimenez.bf@gmail.com>
2016-07-07 23:28:15 +0200

    POST new task

--- src/AppBundle/Controller/TasksController.php
+++ src/AppBundle/Controller/TasksController.php
@@ -3,9 +3,13 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Task;
+use AppBundle\Form\Type\TaskType;
 use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class TasksController extends FOSRestController
 {
@@ -40,8 +44,39 @@ class TasksController extends FOSRestController
      * )
      * @Get("tasks/")
      */
-    public function getAction()
+    public function getTaskAction()
     {
         return $this->getDoctrine()->getRepository('AppBundle:Task')->findAll();
+    }
+
+    /**
+     * Create new task
+     * @var Request $request
+     * @return array
+     * @ApiDoc(
+     *     output="AppBundle\Entity\Task",
+     *     statusCodes={
+     *         200 = "Returned when successful",
+     *         404 = "Returned when the form has errors"
+     *     }
+     * )
+     * @Post("tasks/")
+     */
+    public function postAction(Request $request)
+    {
+        $task = new Task();
+        $form = $this->createForm(new TaskType(), $task);
+        $form->submit($request->request->all());
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+
+            return array("task" => $task);
+        }
+        return array(
+            'form' => $form,
+        );
     }
 }
---
+++ src/AppBundle/Form/Type/TaskType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('description', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'csrf_protection' => false,
+            'data-class' => 'AppBundle\Entity\Task'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'app_task';
+    }
+}
