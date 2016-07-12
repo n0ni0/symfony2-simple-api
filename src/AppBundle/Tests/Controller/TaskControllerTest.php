2fc58c6f27dc411634355a5b608011b27dc1ef18
Antonio Jiménez <ajimenez.bf@gmail.com>
2016-07-12 16:49:21 +0200

    Test post, put and patch with errors

--- src/AppBundle/Tests/Controller/TaskControllerTest.php
+++ src/AppBundle/Tests/Controller/TaskControllerTest.php
@@ -32,6 +32,16 @@ class TaskControllerTest extends AbstractTestCase
         $this->assertJsonResponse($this->client->getResponse(),200);
     }
 
+    public function testCreateTaskWithErrors()
+    {
+        $crawler = $this->client->request('POST', '/tasks',[
+            'title'=>'test task',
+            'xxx'  =>'test description'
+        ]);
+
+        $this->assertJsonResponse($this->client->getResponse(),400);
+    }
+
     public function testPutTask()
     {
         $crawler = $this->client->request('PUT', '/tasks/1',[
@@ -42,6 +52,26 @@ class TaskControllerTest extends AbstractTestCase
         $this->assertJsonResponse($this->client->getResponse(),200);
     }
 
+    public function testPutTaskThatNotExist()
+    {
+        $crawler = $this->client->request('PUT', '/tasks/100',[
+            'title'=>'test task',
+            'description' =>'test description'
+        ]);
+
+        $this->assertJsonResponse($this->client->getResponse(),404);
+    }
+
+    public function testPutTaskWithErrors()
+    {
+        $crawler = $this->client->request('PUT', '/tasks/1',[
+            'title'=>'test task',
+            'xxx'  =>'test description'
+        ]);
+
+        $this->assertJsonResponse($this->client->getResponse(),400);
+    }
+    
     public function testPatchTask()
     {
         $crawler = $this->client->request('PATCH', 'tasks/1',[
@@ -51,11 +81,44 @@ class TaskControllerTest extends AbstractTestCase
         $this->assertJsonResponse($this->client->getResponse(), 200);
     }
 
+    public function testPatchTaskThatNotExist()
+    {
+        $crawler = $this->client->request('PATCH', 'tasks/100',[
+            'title' => 'test task 3'
+        ]);
+
+        $this->assertJsonResponse($this->client->getResponse(), 404);
+    }
+
+    public function testPatchTaskWithErrors()
+    {
+        $crawler = $this->client->request('PATCH', 'tasks/1',[
+            'xxx' => 'test task 3'
+        ]);
+
+        $this->assertJsonResponse($this->client->getResponse(), 400);
+    }
+
     public function testDeleteTask()
     {
         $crawler = $this->client->request('DELETE', '/tasks/1');
 
         $this->assertJsonResponse($this->client->getResponse(),202);
+    }
+
+    public function testDeleteTaskThatNotExist()
+    {
+        $crawler = $this->client->request('DELETE', '/task/100');
+
+        $this->assertJsonResponse($this->client->getResponse(), 404);
+    }
+
+
+    public function testDeleteNullTask()
+    {
+        $crawler = $this->client->request('DELETE', '/task/');
+
+        $this->assertJsonResponse($this->client->getResponse(), 404);
     }
 
     protected function assertJsonResponse($response, $statusCode = 200)
